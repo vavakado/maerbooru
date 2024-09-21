11463b370d906bcba4ebfa9640e4a672c34dbb61
Vladimir Rubin <vavakado@proton.me>
2024-09-21 20:58:49 +0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+# Get started with a build env with Rust nightly
+FROM rustlang/rust:nightly-alpine AS builder
+
+RUN apk update && \
+    apk add --no-cache bash curl npm libc-dev binaryen
+
+RUN npm install -g sass
+
+RUN curl --proto '=https' --tlsv1.2 -LsSf https://github.com/leptos-rs/cargo-leptos/releases/latest/download/cargo-leptos-installer.sh | sh
+
+# Add the WASM target
+RUN rustup target add wasm32-unknown-unknown
+
+WORKDIR /work
+COPY . .
+
+RUN npm install
+RUN cargo leptos build --release -vv
+
+FROM rustlang/rust:nightly-alpine AS runner
+LABEL maintainer="vavakado@proton.me"
+
+WORKDIR /app
+
+COPY --from=builder /work/target/release/maerbooru /app/
+COPY --from=builder /work/target/site /app/site
+COPY --from=builder /work/Cargo.toml /app/
+
+ENV RUST_LOG="info"
+ENV LEPTOS_SITE_ADDR="0.0.0.0:8080"
+ENV LEPTOS_SITE_ROOT=./site
+EXPOSE 8080
+
+CMD ["/app/maerbooru"]
---
+++ src/app.rs
@@ -0,0 +1,61 @@
+use crate::error_template::{AppError, ErrorTemplate};
+use crate::fileupload::FileUpload;
+use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context();
+
+    view! {
+
+
+        // injects a stylesheet into the document <head>
+        // id=leptos means cargo-leptos will hot-reload this stylesheet
+        <Stylesheet id="leptos" href="/pkg/maerbooru.css"/>
+
+        // sets the document title
+        <Title text="Welcome to Leptos"/>
+
+        // content for this welcome page
+        <Router fallback=|| {
+            let mut outside_errors = Errors::default();
+            outside_errors.insert_with_default_key(AppError::NotFound);
+            view! {
+                <ErrorTemplate outside_errors/>
+            }
+            .into_view()
+        }>
+            <main>
+                <Routes>
+                    <Route path="" view=FileUpload/>
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+/// Renders the home page of your application.
+#[component]
+fn HomePage() -> impl IntoView {
+    // Creates a reactive value to update the button
+    let (count, set_count) = create_signal(0);
+    let on_click = move |_| set_count.update(|count| *count += 1);
+
+    view! {
+        <Title text="Leptos + Tailwindcss"/>
+        <main>
+            <div class="bg-gradient-to-tl from-[#a50000] to-blue-900 text-white font-mono flex flex-col min-h-screen">
+                <div class="flex flex-row-reverse flex-wrap m-auto">
+                    <button on:click=on_click class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
+                        "Click number " {count}
+                    </button>
+                </div>
+            </div>
+        </main>
+    }
+}
+
+
